e0dd2284c3e6b5b16ab8c88f8b7f64945dec1376
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-31 11:00:51 -0800

    Move tests outside package

--- {{cookiecutter.package_name}}/tests/test_main.py
+++ {{cookiecutter.package_name}}/tests/test_main.py
@@ -2,12 +2,6 @@
 #
 # main.py
 
-
-from __future__ import division
-from __future__ import print_function
-from __future__ import absolute_import
-from __future__ import unicode_literals
-
 from pytest import mark
 
 from {{cookiecutter.package_name}}.main import math
