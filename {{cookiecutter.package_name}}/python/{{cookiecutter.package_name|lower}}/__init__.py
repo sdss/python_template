be81ddca4315cdeb669d6fc543eac5eae1a1a4e3
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-31 13:09:39 -0800

    Use sdsstools

--- {{cookiecutter.package_name}}/python/{{cookiecutter.package_name|lower}}/__init__.py
+++ {{cookiecutter.package_name}}/python/{{cookiecutter.package_name|lower}}/__init__.py
@@ -1,18 +1,16 @@
 # encoding: utf-8
 
-from .utils import get_config, get_logger
+from sdsstools import get_config, get_logger, get_package_version
 
 
 NAME = '{{cookiecutter.package_name}}'
 
-
 # Loads config
 config = get_config(NAME)
-
 
 # Inits the logging system. Only shell logging, and exception and warning catching.
 # File logging can be started by calling log.start_file_logger(path).
 log = get_logger(NAME)
 
 
-__version__ = '{{cookiecutter.version}}'
+__version__ = get_package_version(path=__file__, package_name={{cookiecutter.pip_name}})
