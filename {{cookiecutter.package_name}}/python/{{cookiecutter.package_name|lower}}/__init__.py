62e8e2a8aa0f980b0fc074d89eb9883e4557b417
José Sánchez-Gallego <gallegoj@uw.edu>
2020-02-27 15:48:52 -0800

    Use package_name for config

--- {{cookiecutter.package_name}}/python/{{cookiecutter.package_name|lower}}/__init__.py
+++ {{cookiecutter.package_name}}/python/{{cookiecutter.package_name|lower}}/__init__.py
@@ -6,7 +6,7 @@ from sdsstools import get_config, get_logger, get_package_version
 NAME = '{{cookiecutter.pip_name}}'
 
 # Loads config
-config = get_config(NAME)
+config = get_config('{{cookiecutter.package_name}}')
 
 # Inits the logging system. Only shell logging, and exception and warning catching.
 # File logging can be started by calling log.start_file_logger(path).
