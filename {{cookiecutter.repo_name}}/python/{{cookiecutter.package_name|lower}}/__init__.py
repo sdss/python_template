cab5181dbd22d9f99908f31076755f5aa146c16d
Brian Cherinka <havok2063@hotmail.com>
2020-05-04 10:02:56 -0400

    adding some comments for post-install context

--- {{cookiecutter.repo_name}}/python/{{cookiecutter.package_name|lower}}/__init__.py
+++ {{cookiecutter.repo_name}}/python/{{cookiecutter.package_name|lower}}/__init__.py
@@ -2,15 +2,17 @@
 
 from sdsstools import get_config, get_logger, get_package_version
 
-
+# pip package name
 NAME = '{{cookiecutter.pip_name}}'
 
-# Loads config
+# Loads config. config name is the package name.
 config = get_config('{{cookiecutter.package_name}}')
 
-# Inits the logging system. Only shell logging, and exception and warning catching.
-# File logging can be started by calling log.start_file_logger(path).
+# Inits the logging system as NAME. Only shell logging, and exception and warning catching.
+# File logging can be started by calling log.start_file_logger(path).  Filename can be different
+# than NAME.
 log = get_logger(NAME)
 
 
+# package name should be pip package name
 __version__ = get_package_version(path=__file__, package_name=NAME)
