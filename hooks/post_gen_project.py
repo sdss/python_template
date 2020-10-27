2f63f264e20a097a1d162aafa390a779e5e60680
José Sánchez-Gallego <gallegoj@uw.edu>
2020-10-27 10:49:36 -0700

    Change reference to cli.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -40,7 +40,7 @@ def copy_packaging_system():
 
     files = os.listdir(pack_dir)
     for file_ in files:
-        if file_ == 'cli.py':
+        if file_ == '__main__.py':
             shutil.move(os.path.join(pack_dir, file_), os.path.join(PYTHONDIR, PKGNAME))
         else:
             shutil.move(os.path.join(pack_dir, file_), CURRENTDIR)
